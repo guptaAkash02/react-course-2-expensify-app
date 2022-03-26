663e4cdd5acdf21d82369051cc9d2f1f859b042e
akashkskygupta022000 <99875711+akashkskygupta022000@users.noreply.github.com>
2022-03-26 15:19:06 +0530

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path= require('path');
 
- module.exports={
+
+module.exports=(env)=>{
+    const isProduction = env === 'production';
+   return{
     entry:"./src/app.js",
     output:{
         path:path.join(__dirname,'public'),
@@ -22,9 +25,11 @@ const path= require('path');
         }
     ]
     },
-    devtool:'cheap-module-eval-source-map',
+    devtool:isProduction?'source-map':'cheap-module-eval-source-map',
     devServer: {
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true
     }
-};
+   }
+}
+ 
